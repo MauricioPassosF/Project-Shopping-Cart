e8d456a17d0b29585360720287db80ccc45bfc40
MauricioPassosF <mauricio.passos97@gmail.com>
2023-03-23 19:29:56 -0300

    requisito 9

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
-import { fetchProductsList } from './helpers/fetchFunctions';
-import { createProductElement } from './helpers/shopFunctions';
+import { fetchProduct, fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement, createCartProductElement } from './helpers/shopFunctions';
+import { getSavedCartIDs } from './helpers/cartFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 const h2 = document.createElement('h2');
 const products = document.querySelector('.products');
+const cartProducts = document.querySelector('.cart__products');
 
+const addCartOnload = async () => {
+  const prodInfos = await Promise.all(getSavedCartIDs().map((id) => (fetchProduct(id))));
+  prodInfos
+    .forEach((info) => cartProducts.appendChild(createCartProductElement(info)));
+};
 const addText = (text, addClass) => {
   h2.innerHTML = text;
   h2.className = addClass;
@@ -27,6 +34,7 @@ const getProducts = async (search) => {
     addText('Algum erro ocorreu, recarregue a página e tente novamente', 'error');
   } finally { loadingRemove(); }
 };
+addCartOnload();
 getProducts('computador');
 // const resultado = await fetchProduct('MLB1405519561');
 // const pegarId = await fetchProductsList('computador');
