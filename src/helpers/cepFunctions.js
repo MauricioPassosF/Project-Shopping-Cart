98f7955c73ff5fafe2743485177649699de23986
MauricioPassosF <mauricio.passos97@gmail.com>
2023-03-24 18:21:44 -0300

    Requisitos finalizados

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,44 +1,25 @@
-const spanAdress = document.querySelector('.cart__address');
-// const fetchAddress = async (URL) => {
-//   const result = await fetch(URL);
-//   const data = await result.json();
-//   return data;
-// };
-
 export const getAddress = async (cep) => {
-  // let address;
-  try {
-    const result = await Promise.any([
-      fetch(`https://cep.awesomeapi.com.br/json/${cep}`),
-      fetch(`https://brasilapi.com.br/api/cep/v2/${cep}`),
-    ]);
-    const address = await result.json();
-    console.log('try cima');
-    console.log(address);
-    return address;
-  } catch (error) {
-  // const error1 = 404;
-  // const error2 = 400;
-  // if (address.status === error1 || address.status === error2) {
-    console.log('catch cima');
-    spanAdress.innerHTML = 'CEP não';
-    // address = 'CEP não encontrado';
-  // } finally {
-  //   return address
-  // }
-  // } catch (error) {
-  //   return error;
+  const result = await Promise.any([
+    fetch(`https://cep.awesomeapi.com.br/json/${cep}`),
+    fetch(`https://brasilapi.com.br/api/cep/v2/${cep}`),
+  ]);
+  const address = await result.json();
+  const error1 = 404;
+  const error2 = 400;
+  if (address.status === error1 || address.status === error2) {
+    throw new Error('CEP não encontrado');
   }
+  return address;
 };
 
 export const searchCep = async () => {
+  const spanAdress = document.querySelector('.cart__address');
   try {
     const data = await getAddress(document.querySelector('.cep-input').value);
     spanAdress.innerHTML = data.address_type
       ? `${data.address} - ${data.district} - ${data.city} - ${data.state}`
       : `${data.street} - ${data.neighborhood} - ${data.city} - ${data.state}`;
   } catch (error) {
-    console.log('catch baixo');
     spanAdress.innerHTML = 'CEP não encontrado';
   }
 };
